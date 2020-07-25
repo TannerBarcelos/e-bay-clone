911c1cea9844dcac147f58d1bc18b1d4a6b03203
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-07-25 15:23:51 -0700

    add

---
+++ client/build/precache-manifest.ab16e506a9a60cf096464d03cb069028.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35d78d660bb7205751531be63943e196",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f46138b2c5b82bd828c1",
+    "url": "/static/css/main.f7eef90e.chunk.css"
+  },
+  {
+    "revision": "d3e583609ae2b07b040b",
+    "url": "/static/js/2.a891ea2c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a891ea2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f46138b2c5b82bd828c1",
+    "url": "/static/js/main.616e01ac.chunk.js"
+  },
+  {
+    "revision": "39d502c0600acb131b17",
+    "url": "/static/js/runtime-main.a3934485.js"
+  }
+]);
