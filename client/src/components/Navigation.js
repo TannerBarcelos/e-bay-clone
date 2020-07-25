fdd7aacb98a4697fdfa1835e27b3cd54bbcd9b54
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-07-25 15:52:30 -0700

    styles and errors

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import {
+  NavLink
+} from 'react-router-dom';
 
 import '../App.css';
-import {connect} from 'react-redux';
+import {
+  connect
+} from 'react-redux';
 
 //navigation component: builds out the linkage for the routes within the react app (will be mounted at the top level app)
 const Navigation = props => {
   return (
-    <header>
+    <header style={{ "position": "sticky", "top": "0", "zIndex": "1000" }}>
       <nav>
-        {/* will be using routing tool NavLink*/}
         <ul className="ul-nav-container">
           <li>
             <NavLink to="/" id="links">fakebay</NavLink>
           </li>
           <li>
             <NavLink to="/cart" id="float-right-links">
-              <i
-                className="fas fa-shopping-cart"
-                data-count={props.cart.length}
-              />
+              <i className="fas fa-shopping-cart" data-count={props.cart.length} />
             </NavLink>
           </li>
         </ul>
@@ -30,7 +30,9 @@ const Navigation = props => {
 
 //we will render the current cart length in the icon for data-count in the icon
 const mapStateToProps = state => {
-  return {cart: state.currentCart};
+  return {
+    cart: state.currentCart
+  };
 };
 
-export default connect (mapStateToProps) (Navigation);
+export default connect(mapStateToProps)(Navigation);
